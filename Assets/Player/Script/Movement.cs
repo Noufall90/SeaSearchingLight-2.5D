331c1d046250386b4fcebde2ae0d431432f3c7db
Noval ian <andrianau26@gmail.com>
2023-12-06 20:30:16 +0800

    mau spirnt

--- Assets/Player/Script/Movement.cs
+++ Assets/Player/Script/Movement.cs
@@ -5,36 +5,43 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public ParticleSystem DustRun;
-    
+
     public float speed = 5f;
-    public float sprintSpeed;
+    public float sprintSpeed = 10f; // Assign a value to sprintSpeed
+
     private bool isSprinting = false;
 
-    void Update() 
+    void Update()
     {
-        float moveSpeed = speed;
-        Vector3 moveDirection = Vector3.zero; // Initializing moveDirection
-        
-        if (Input.GetKey(KeyCode.LeftShift)) {
-            CreateDustRun();
-            moveSpeed = sprintSpeed;
+        // Get input for sprinting
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
             isSprinting = true;
         }
-        else {
+        else
+        {
             isSprinting = false;
         }
-        
+
         // Get input for movement
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        
+
         // Calculate the movement direction
-        moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized * moveSpeed * Time.deltaTime;
-        
-        // Move the GameObject
-        transform.Translate(moveDirection);
+        Vector3 moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized * Time.deltaTime;
+
+        // Check if sprinting and adjust speed accordingly
+        if (isSprinting)
+        {
+            CreateDustRun();
+            transform.Translate(moveDirection * sprintSpeed);
+        }
+        else
+        {
+            transform.Translate(moveDirection * speed);
+        }
     }
-    
+
     void CreateDustRun()
     {
         DustRun.Play();
