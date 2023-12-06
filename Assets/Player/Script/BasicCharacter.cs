dd49bbdc9a6807eea642598de7a5d73492a31aff
Noval ian <andrianau26@gmail.com>
2023-12-06 22:19:16 +0800

    test lari wasd

--- Assets/Player/Script/BasicCharacter.cs
+++ Assets/Player/Script/BasicCharacter.cs
@@ -3,76 +3,71 @@ using UnityEngine;
 
 namespace Common.Scripts
 {
-  public class BasicCharacter : MonoBehaviour
-  {
-    #region Constants
-
-    private static readonly int WALK_PROPERTY = Animator.StringToHash("Walk");
-
-    #endregion
-
-
-    #region Inspector
-
-    [SerializeField]
-    private float speed = 2f;
-
-    [Header("Relations")]
-    [SerializeField]
-    private Animator animator = null;
-
-    [SerializeField]
-    private Rigidbody physicsBody = null;
-
-    [SerializeField]
-    private SpriteRenderer spriteRenderer = null;
-
-    #endregion
-
-
-    #region Fields
-
-    private Vector3 _movement;
-
-    #endregion
-
-
-    #region MonoBehaviour
-
-    private void Update ()
+    public class BasicCharacter : MonoBehaviour
     {
-      // Vertical
-      float inputY = 0;
-      if ( Input.GetKey(KeyCode.UpArrow) )
-        inputY = 1;
-      else if ( Input.GetKey(KeyCode.DownArrow) )
-        inputY = -1;
+        private static readonly int WALK_PROPERTY = Animator.StringToHash("Walk");
 
-      // Horizontal
-      float inputX = 0;
-      if ( Input.GetKey(KeyCode.RightArrow) )
-      {
-        inputX = 1;
-        spriteRenderer.flipX = false;
-      }
-      else if ( Input.GetKey(KeyCode.LeftArrow) )
-      {
-        inputX = -1;
-        spriteRenderer.flipX = true;
-      }
+        [SerializeField] private float speed = 10f;
+        [SerializeField] private float sprintSpeed = 15f;
+        [SerializeField] private float raycastDistance = 1f; // Distance from the object to the ground
+        [Header("Relations")]
+        [SerializeField] private Animator animator = null;
+        [SerializeField] private Rigidbody physicsBody = null;
+        [SerializeField] private SpriteRenderer spriteRenderer = null;
 
-      // Normalize
-      _movement = new Vector3(inputX, 0, inputY).normalized;
+        private Vector3 _movement;
+        private bool isSprinting = false;
 
-      animator.SetBool(WALK_PROPERTY,
-                       Math.Abs(_movement.sqrMagnitude) > Mathf.Epsilon);
+        private void Update()
+        {
+            // Vertical
+            float inputY = 0;
+            if (Input.GetKey(KeyCode.W))
+                inputY = 1;
+            else if (Input.GetKey(KeyCode.S))
+                inputY = -1;
+
+            // Horizontal
+            float inputX = 0;
+            if (Input.GetKey(KeyCode.D))
+            {
+                inputX = 1;
+                spriteRenderer.flipX = false;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                inputX = -1;
+                spriteRenderer.flipX = true;
+            }
+
+            // Normalize
+            _movement = new Vector3(inputX, 0, inputY).normalized;
+
+            // Sprint mechanic
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                isSprinting = true;
+            }
+            else
+            {
+                isSprinting = false;
+            }
+
+            float currentSpeed = isSprinting ? sprintSpeed : speed;
+
+            // Update animator based on movement
+            animator.SetBool(WALK_PROPERTY, Math.Abs(_movement.sqrMagnitude) > Mathf.Epsilon);
+
+            // Move the player
+            physicsBody.velocity = _movement * currentSpeed;
+
+            // Maintain the object on the ground using Raycast
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Vector3.down, out hit, raycastDistance))
+            {
+                float distanceToGround = hit.distance;
+                transform.position = new Vector3(transform.position.x, transform.position.y - distanceToGround, transform.position.z);
+            }
+        }
     }
-
-    private void FixedUpdate ()
-    {
-      physicsBody.velocity = _movement * speed;
-    }
-
-    #endregion
-  }
 }
